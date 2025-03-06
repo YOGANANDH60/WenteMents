322675a0046c774802459e4328c51a798d6efddf
YOGANANDH60 <yoga132005@gmail.com>
2025-03-07 00:40:50 +0530

    changes

--- tfinal-project/settings.py
+++ tfinal-project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1(729z!5h-5z0@tf74q%587o!0!8$&38xim+yr715i1hm9xone
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
